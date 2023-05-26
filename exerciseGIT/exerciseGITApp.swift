e1a8d38134c630d90c867dce262950d6619684c3
baraboy <rizky.dwiixg36@gmail.com>
2023-05-26 15:41:04 +0700

    Initial Commit

---
+++ exerciseGIT/exerciseGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseGITApp.swift
+//  exerciseGIT
+//
+//  Created by Rizky Dwi Hadisaputro on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct exerciseGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
